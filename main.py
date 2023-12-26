297a9380fa49ecc230b896943f2c369438fd1bd1
Chet25 <chetan25december@gmail.com>
2023-12-26 17:09:37 +0530

    chat save

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+import motor.motor_asyncio
 import socketio
+import datetime
 
 app = FastAPI()
 
 origins = [
     "http://localhost:3000", "https://alumni-mapping-system.vercel.app"
 ]
+
+uri = "mongodb+srv://chirag1292003:12092003Duan@alumni-mapping-system-d.iryfq1v.mongodb.net/?retryWrites=true&w=majority"
+client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+database = client.alumni_mapping_system
+chat_collection = database.chat
 
 sio=socketio.AsyncServer(cors_allowed_origins=origins,async_mode='asgi')
 socket_app = socketio.ASGIApp(sio)
@@ -16,5 +23,11 @@ def read_root():
     return {"Chat": "Opened"}
 
 @sio.on('msg')
-async def client_side_receive_msg(sid, msg):
-    await sio.emit("msg", str(msg))
+async def client_side_receive_msg(sid, msg, student, alumni):
+    try:
+        chat_collection.update_one(
+            {"alumni": alumni}, {"$push": {"chat": {"time": datetime.datetime.now(), "chat": msg, "sender": student}}}
+        )
+        await sio.emit("msg", str(msg))
+    except:
+        pass
