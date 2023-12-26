911e7be694258d85e300f07537a503a088714dc5
Chet25 <chetan25december@gmail.com>
2023-12-26 15:13:46 +0530

    db removal

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI
 import socketio
-import motor.motor_asyncio
-import datetime
 
 app = FastAPI()
-
-# uri = os.getenv("MONGODB")
-uri = "mongodb+srv://chirag1292003:12092003Duan@alumni-mapping-system-d.iryfq1v.mongodb.net/?retryWrites=true&w=majority"
-client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-database = client.alumni_mapping_system
-chat_collection = database.chat
-
-print(datetime.datetime.now())
 
 origins = [
     "http://localhost:3000", "https://alumni-mapping-system.vercel.app"
@@ -22,21 +12,9 @@ socket_app = socketio.ASGIApp(sio)
 app.mount("/", socket_app)
 
 @app.get("/")
-async def read_root():
+def read_root():
     return {"Chat": "Opened"}
 
-@sio.on('fetch')
-async def get_intial_data(sid, alumni):
-    data = await chat_collection.find_one({"alumni": alumni})
-    print(data)
-
 @sio.on('msg')
-async def client_side_receive_msg(sid, msg, alumni, student):
-    chat_collection.update_one(
-        {"alumni": alumni}, {"$push": {"chat": {
-            "time": datetime.datetime.now(),
-            "sender": student,
-            "chat": msg
-        }}}
-    )
+async def client_side_receive_msg(sid, msg):
     await sio.emit("msg", str(msg))
